18231dbad5c55565a752b4d096f815ef15f399d9
Philip M-L <philip.marshalllockyer@outlook.com>
2023-09-14 08:15:03 +0100

    Finished creating all CRUD routes needed for the project

--- backend/index.js
+++ backend/index.js
@@ -90,6 +90,19 @@ app.put("/books/:id", async (req, res) => {
   }
 });
 
+// Route to delete a specific book by ID
+app.delete("/book/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findByIdAndDelete(id);
+    if (!book) return res.status(404).json({ message: "Book not found" });
+    res.status(200).json({ message: "Book successfully deleted" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error.message);
+  }
+});
+
 // LAUNCHING THE SERVER
 mongoose
   .connect(mongoDBURL)
